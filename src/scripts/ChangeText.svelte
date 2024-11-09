ac53e608f15559fd73e5b76ad23473aaec430b26
LixFerox <144841907+LixFerox@users.noreply.github.com>
2024-11-09 06:04:42 +0100

    Upload new Files

---
+++ src/scripts/ChangeText.svelte
@@ -0,0 +1,16 @@
+<!--PUEDES BORRAR ESTO, ES UN EJEMPLO DE USO DE SVELTE-->
+<script>
+    let text = "Bienvenido a tu página web";
+  
+    function changeText() {
+      if (text=="Bienvenido a tu página web") {
+        text = "Esto es una prueba";
+      }else {
+        text = "Bienvenido a tu página web";
+      }
+    }
+  </script>
+  
+  <h1>{text}</h1>
+  <button class="bg-green-500 p-2" on:click={changeText}>¿Qué es esto?</button>
+  
