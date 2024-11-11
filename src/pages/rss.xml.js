fdb24d7bd0066326a131137e23387ac57b5dfb28
LixFerox <144841907+LixFerox@users.noreply.github.com>
2024-11-11 20:36:37 +0100

    Final Update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,20 @@
-import {SITE_NAME, SITE_DESCRIPTION, SITE_LANGUAGE } from '../variables/config';
+import {SITE_NAME, SITE_DESCRIPTION, SITE_LANGUAGE, SITE_AUTHOR } from '../variables/config';
 import rss from '@astrojs/rss';
-
+import post from '../../data/users.json';
 export function GET(context) {
   return rss({
     title: `${SITE_NAME} - Blog`,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: [],
+    items: post.map(p => ({
+      title: p.title,
+      link: `${context.site}/blog/${p.id}`,
+      description: p.description,
+      pubDate: new Date(p['pub-date']).toUTCString(),
+      guid: `${context.site}/blog/${p.id}`,
+      author: SITE_AUTHOR,
+      category: p.category,
+    })),
     customData: `<language>${SITE_LANGUAGE}</language>`,
   });
 }
