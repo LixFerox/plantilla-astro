e25dcdb5028ad255eacbb4342e69e7cae8e68047
LixFerox <144841907+LixFerox@users.noreply.github.com>
2024-12-11 21:00:03 +0100

    delete project

--- components/Year.svelte
+++
@@ -1,4 +0,0 @@
-<script>
-var year=new Date().getFullYear();
-</script>
-<p class="sm:text-base text-sm font-semibold italic">{year}</p>
--- src/variables/config.ts
+++
@@ -1,8 +0,0 @@
-export const optionSEO={
-    favicon: '/favicon.svg',
-    description: 'Plantilla Astro que podrás usar como base para tu proyecto',
-    author: 'Tu Nombre',
-    url: 'https://tu-url.com',
-    nameWeb: 'MiPlantilla',
-    domain: 'tu-dominio.com'
-}
