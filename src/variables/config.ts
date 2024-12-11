71ba9c3e01b5426f07944c363c0a03074360f7d6
LixFerox <144841907+LixFerox@users.noreply.github.com>
2024-12-11 07:10:52 +0100

    delete files

--- src/variables/config.ts
+++
@@ -1,7 +0,0 @@
-export const SITE_DNS= "tudominio.com"
-export const SITE_WEBSITE= `https://${SITE_DNS}`
-export const SITE_AUTHOR= "YourName"
-export const SITE_FAVICON= "/favicon.svg"
-export const SITE_DESCRIPTION= "AVANCED_DESCRIPTION"
-export const SITE_NAME= "COMPANY_NAME"
-export const SITE_LANGUAGE= "es-es"
